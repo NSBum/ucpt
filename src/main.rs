61781bbc389c4617ec7491eee3a55ea1656c2fa5
NSBum <duncan.alan@me.com>
2023-11-15 06:25:14 -0500

    Initial add/commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::env;
+use std::io::{self, Write};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let character = if args.len() > 1 {
+        args[1].chars().next().expect("No character found")
+    } else {
+        print!("Enter a character: ");
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+        input.trim().chars().next().expect("No character found")
+    };
+
+    let unicode_value = format!("U+{:04X}", character as u32);
+    println!("{}", unicode_value);
+}
+
